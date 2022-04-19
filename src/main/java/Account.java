67dc1eaaa49521a94ed8652cd893b52de7460fb0
slovak1990 <timur.muginov1990@gmail.com>
2022-04-19 09:12:31 +0300

    "third commit"

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -10,12 +10,6 @@ public class Account {
 
     @Step("The method of checking the string for compliance with the requirements")
     public boolean checkNameToEmboss() {
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-             trim() удаляет пробелы в начале и в конце
-             replaceAll("\\s+", " ") убирает лишние пробелы в 1
-         */
         if (name.length() >= 3 && name.length() <= 19 && name.trim().replaceAll("\\s+", " ").contains(" ")) {
             return true;
         } else {
---
+++ src/test/java/ParametersTest.java
@@ -0,0 +1,55 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@Epic("Checking the output of the input of the first and last name to the bank card")
+@Feature("Boundary values and equivalence classes")
+@RunWith(Parameterized.class)
+public class ParametersTest {
+
+    private final String value;
+    private final boolean expectedResponse;
+
+    public ParametersTest(String value, boolean expectedResponse){
+        this.value = value;
+        this.expectedResponse = expectedResponse;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: входная строка {0}, валидно: {1}")
+    public static Object[] maneParameters() {
+        return new Object[][]{
+                {"Тимоти Шаламе", true},
+                {"ТИМОТИ ШАЛАМЕ", true},
+                {" Тимоти Шаламе ", true},
+                {"Тимоти    Шаламе", true},
+                {"Та и", true},
+                {"Грандмастер Адмирал", true},
+                {"Гранмастер Адмирал", true},
+                {"Ти", false},
+                {"Грандмастер Адмиралъ", false},
+                {"", false},
+                {"Тимотейка Шаламойшниковайте", false},
+                {"Тимоти-Шаламе", false},
+                {"1234567891123456789", false},
+                {"           ", false},
+        };
+    }
+
+    @Test
+    @DisplayName("Checking verification")
+    @Description("Parameterized test with values")
+    public void withParametersTest() {
+        Account account = new Account(value);
+
+        boolean actual = account.checkNameToEmboss();
+
+        assertEquals(expectedResponse, actual);
+    }
+
+
+}
